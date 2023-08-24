377d66171096f13cf5b008160c41290a3af75c6f
vitorribeiro99 <v99ribeiro@gmail.com>
2023-08-24 01:29:35 +0100

    fix: removed text

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -18,14 +18,3 @@ export const aboutLinks = [
   { id: "vision", title: "Vision" },
   { id: "team", title: "Team" },
 ];
-
-export const slogans = [
-  { id: "slogan2", title: "Bringing Science from Web0 to" },
-  { id: "slogan2.1", title: "Web3" },
-  { id: "slogan1", title: "Decentralizing Scientific Publishing" },
-  {
-    id: "invitation",
-    title:
-      "Take control of your scientific contributions! Unlock the potential of your research with our decentralized, transparent, and collaborative platform",
-  },
-];
