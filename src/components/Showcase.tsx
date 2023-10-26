5aa9a776c48b37b5ab1cb6d2cb06cccffa3379bc
vitorribeiro99 <v99ribeiro@gmail.com>
2023-10-26 11:09:26 +0100

    Update Showcase.tsx

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -4,7 +4,7 @@ const Showcase = () => {
   return (
     <section
       id="showcase"
-      className="bg-mshowcase md:bg-showcase bg-100% bg-no-repeat relative flex flex-col items-center h-[585px] sm:h-[730px]"
+      className="bg-mshowcase md:bg-showcase bg-100% bg-no-repeat relative flex flex-col items-center xs:h-[600px] ss:h-[950px] sm:h-[1150px] md:h-[700px]  lg:h-[800px] xl:h-[1150px]"
     ></section>
   );
 };
