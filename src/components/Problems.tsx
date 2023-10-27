42654cd1c46162eb00ca8e7977bf669114edfa76
vitorribeiro99 <v99ribeiro@gmail.com>
2023-10-27 23:21:50 +0100

    Update Problems.tsx

--- src/components/Problems.tsx
+++ src/components/Problems.tsx
@@ -13,7 +13,7 @@ const Problems = () => {
   return (
     <section id="problems" className={`relative grid grid-cols-2`}>
       <div
-        className={`bg-dark-blue-60 flex flex-col justify-center items-center w-full gap-12 py-12 px-5 ss:py-16 ss:pr-15 ss:pl-10`}
+        className={`bg-dark-blue-60 flex flex-col justify-center items-center w-full gap-12 py-12 px-4 ss:py-16 ss:pr-15 ss:pl-10`}
       >
         <p className="text-default-0 text-2xl md:text-5xl leading-9 md:leading-[60px] font-medium">
           Problems
@@ -84,7 +84,7 @@ const Problems = () => {
         </circle>
       </div>
       <div
-        className={`bg-blue-20 flex flex-col justify-center items-center w-full gap-12 py-12 px-5 ss:py-16 ss:pl-15 ss:pr-10`}
+        className={`bg-blue-20 flex flex-col justify-center items-center w-full gap-12 py-12 px-4 ss:py-16 ss:pl-15 ss:pr-10`}
       >
         <p className="text-dark-blue-60 text-2xl md:text-5xl leading-9 md:leading-[60px] font-medium">
           Solutions
