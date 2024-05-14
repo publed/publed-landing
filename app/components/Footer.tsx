2c8ad5107a5dbc8e69c9ad6254cc8cdd2dd7a4f1
Miguel Ângelo Alves de Freitas <61701003+MrNameless10@users.noreply.github.com>
2024-05-14 18:01:47 +0100

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -25,7 +25,7 @@ const Footer = () => {
           <Link href={'https://www.linkedin.com/company/publ3d/'}>
             <Linkedin className="fill-[#4D6983] transition-colors duration-300 hover:fill-gray-600" />
           </Link>
-          <Link href={'https://discord.gg/tYgZc49R'}>
+          <Link href={'https://discord.gg/kpRCPgC6uB'}>
             <Discord className="fill-[#4D6983] transition-colors duration-300 hover:fill-gray-600" />
           </Link>
         </div>
