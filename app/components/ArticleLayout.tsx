c03474f1ef333473a957f5f285e6ffdb6c455e0d
vitorribeiro99 <73917262+vitorribeiro99@users.noreply.github.com>
2024-05-17 18:01:04 +0100

    tsts

--- app/components/ArticleLayout.tsx
+++ app/components/ArticleLayout.tsx
@@ -17,9 +17,34 @@ import { useEffect, useState } from 'react';
 import Head from 'next/head';
 
 export const metadata: Metadata = {
-  title: 'Article | Publed',
+  title: 'Blog | Publed',
+  metadataBase: new URL('https://www.publed.io/'),
   description:
-    'Read the latest articles from Publed and stay up-to-date with the latest trends in the space.',
+    'Find out more about Publed and stay tuned for all the announcements and information from our experts in the space.',
+  keywords:
+    'Science, Education, Knowledge, DeSci, Web, Web3, SOL, Solana, mint, collection, Knowledge Object, KO, Artifacts, Papers, Scientific Publishing, Publishing',
+  openGraph: {
+    type: 'website',
+    title: 'Blog | Publed',
+    description:
+      'Find out more about Publed and stay tuned for all the announcements and information from our experts in the space.',
+    images: 'hero_publed.png',
+    url: 'https://www.publed.io/',
+    siteName: 'Publed',
+  },
+  appleWebApp: {
+    title: 'Blog | Publed',
+    startupImage: 'logo_twitter.png',
+  },
+  twitter: {
+    title: 'Blog | Publed',
+    description:
+      'Find out more about Publed and stay tuned for all the announcements and information from our experts in the space.',
+    card: 'summary_large_image',
+    site: '@publed_official',
+    creator: '@publed_official',
+    images: 'hero_publed.png',
+  },
 };
 
 function ArrowLeftIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
